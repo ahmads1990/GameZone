bc9e8eefd2887eb1798adc3f6cf4df8e8b15e6c4
Ahmad <ahmads20002000@gmail.com>
2024-04-08 12:48:45 +0200

    Add form get

--- GameZone/Controllers/GamesController.cs
+++ GameZone/Controllers/GamesController.cs
@@ -1,17 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace GameZone.Controllers;
 
-namespace GameZone.Controllers
+public class GamesController : Controller
 {
-    public class GamesController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public GamesController(ApplicationDbContext context)
     {
-        public IActionResult Index()
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        CreateGameFormViewModel viewModel = new()
         {
-            return View();
-        }
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+            Categories = _context.Categories
+            .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+            .OrderBy(c=>c.Text)
+            .ToList(),
+            Devices = _context.Devices
+            .Select(d => new SelectListItem { Value = d.Id.ToString(), Text = d.Name })
+            .OrderBy(d => d.Text)
+            .ToList(),
+        };
+
+        return View(viewModel);
     }
 }
